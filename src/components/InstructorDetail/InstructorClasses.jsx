ab1f67cefcff5760672deb94e5cd97b0feecaa10
Andrew Leck <Aleck360@gmail.com>
2022-07-25 14:18:30 -0500

    center the profile header and created a componet for  Classes

---
+++ src/components/InstructorDetail/InstructorClasses.jsx
@@ -0,0 +1,13 @@
+function instructorClasses() {
+  return (
+    <div className="card">
+      <div className="container">
+        <div> HIT </div>
+        <div> Monday </div>
+        <div> 7:00pm </div>
+      </div>
+    </div>
+  );
+}
+
+export default instructorClasses;
--- src/components/InstructorDetail/InstructorDetail.jsx
+++ src/components/InstructorDetail/InstructorDetail.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import "./InstructorDetail.css";
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import InstructorClasses from "./InstructorClasses";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 function InstructorDetail() {
   return (
@@ -15,26 +16,26 @@ function InstructorDetail() {
         <div className="containerProfile">
           <div> Edan </div>
           <div> He/Him </div>
-          <div> <InstagramIcon /> <FacebookIcon /> <TwitterIcon /> </div>
+          <div>
+            <InstagramIcon color="primary" /> <FacebookIcon color="primary" />
+            <TwitterIcon color="primary" />
+          </div>
           <div className="chip"> Ace Certification </div>
         </div>
       </div>
       <div>
         <h3> Classes </h3>
-        <div className="card">
-          <div className="container">
-            <div> HIT </div>
-            <div> Monday </div>
-            <div> 7:00pm </div>
-          </div>
-        </div>
+        <InstructorClasses />
       </div>
       <h3> Tags </h3>
       <div className="chip">LGBTQ Friendly</div>
       <div>
         <h3> About </h3>
       </div>
-      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <textarea name="" id="" cols="30" rows="10">
+        Hello World! My name is Edan and I have been a trainer for about 10
+        years. I love help people achieve their fitness goals....
+      </textarea>
     </>
   );
 }
