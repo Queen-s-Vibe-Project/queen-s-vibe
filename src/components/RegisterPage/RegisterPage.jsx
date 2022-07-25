a86994c957ef61b8cad774fc86303f59be3b9137
Savon Huynh <xqhuynh@gmail.com>
2022-07-24 19:56:44 -0500

    Style registration form

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { useHistory } from 'react-router-dom';
-import RegisterForm from '../RegisterForm/RegisterForm';
+import { useHistory } from "react-router-dom";
+import RegisterForm from "../RegisterForm/RegisterForm";
 
 function RegisterPage() {
   const history = useHistory();
@@ -11,11 +11,12 @@ function RegisterPage() {
       <RegisterForm />
 
       <center>
+        <h3>Already registered? </h3>
         <button
           type="button"
           className="btn btn_asLink"
           onClick={() => {
-            history.push('/login');
+            history.push("/login");
           }}
         >
           Login
