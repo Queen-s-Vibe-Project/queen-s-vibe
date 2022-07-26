fa40793c2a33e8f000bde46a9ed3de6c19e8d18d
Savon Huynh <xqhuynh@gmail.com>
2022-07-26 10:31:12 -0500

    Style landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -7,7 +7,7 @@ import InstructorCard from "../InstructorCard/InstructorCard";
 function LandingPage() {
   return (
     <>
-      <h3 className="landing-header">Landing Page</h3>
+      <h3 className="landing-header">Welcome to Queen Vibes</h3>
       <h4 className="popular-tags">Search for instructors by tags</h4>
       <SearchBar />
       <h3 className="featured-instructors">Featured Instructors</h3>
