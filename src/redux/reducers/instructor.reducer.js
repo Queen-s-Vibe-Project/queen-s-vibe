7be1788bcaab59e159e0bf3783a10f5463a42976
Al Camara <Alhousseinec@gmail.com>
2022-07-28 12:02:24 -0500

    added favoriteInstructor reducer function

--- src/redux/reducers/instructor.reducer.js
+++ src/redux/reducers/instructor.reducer.js
@@ -15,4 +15,15 @@ export const recommendInstructor = (state = [], action) =>{
             return state;
     }
 }
+
+export const favoriteInstructor = (state =[], action) =>{
+    switch (action.type) {
+        case 'SET_FAVORITE_INSTRUCTOR':
+            
+            return action.payload;
+    
+        default:
+            return state;
+    }
+}
 export default instructorReducer;
