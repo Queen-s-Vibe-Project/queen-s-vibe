b7458bb138fc6c181b28cca8e92c82b45d0c2867
Al Camara <Alhousseinec@gmail.com>
2022-07-28 09:17:41 -0500

    added useEffect and useSelector on recommend

--- src/components/RecommendInstructors/RecommendInstructors.jsx
+++ src/components/RecommendInstructors/RecommendInstructors.jsx
@@ -1,7 +1,17 @@
 import './RecommendInstructors.css'
 import RecommendCard from './RecommendCard'
+import { useEffect } from 'react';
+import { useDispatch,useSelector } from 'react-redux';
+
 
 export default function RecommendInstructors (){
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch({
+            type:'FETCH_RECOMMEND_INSTRUCTOR'
+        })
+    })
+
     return (
         <div className="recommendInstructor">
             <h3 className='recommendInstructor-header'>Recommended Instuctor</h3>
