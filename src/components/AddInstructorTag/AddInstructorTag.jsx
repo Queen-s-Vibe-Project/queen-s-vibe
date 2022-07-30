9e180058e98d94a67cdde95f1b28185faa242f50
Al Camara <Alhousseinec@gmail.com>
2022-07-29 23:53:33 -0500

    add mui

--- src/components/AddInstructorTag/AddInstructorTag.jsx
+++ src/components/AddInstructorTag/AddInstructorTag.jsx
@@ -1,8 +1,20 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
-import AddIcon from '@mui/icons-material/Add';
+import PropTypes from 'prop-types';
 
-export default function AddInstructorTag(){
+
+//MUI
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Dialog from '@mui/material/Dialog';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+export default function AddInstructorTag(props){
     const dispatch = useDispatch()
 
     useEffect(()=>{
