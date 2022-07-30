54fc489323e6556f10b3a8177f2241a6f4619e3b
Al Camara <Alhousseinec@gmail.com>
2022-07-30 00:52:42 -0500

    added tags to modal

--- src/components/AddInstructorTag/AddInstructorTag.jsx
+++ src/components/AddInstructorTag/AddInstructorTag.jsx
@@ -1,23 +1,41 @@
-import { useEffect } from 'react';
+import * as React from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import PropTypes from 'prop-types';
 
 
 //MUI
-import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Dialog from '@mui/material/Dialog';
-import RadioGroup from '@mui/material/RadioGroup';
-import Radio from '@mui/material/Radio';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 
 export default function AddInstructorTag(props){
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 200,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+
+      
+
     const dispatch = useDispatch()
 
-    useEffect(()=>{
+    React.useEffect(()=>{
         dispatch({
             type:"FETCH_TAGS"
           })
@@ -25,12 +43,31 @@ export default function AddInstructorTag(props){
 
     const tags = useSelector((store)=> store.search.tags)
     return(
-        <div
-            onClick={()=>{
-                console.log('click');
-            }}
-        >
-            <AddIcon/>
+        <div>
+            <Button onClick={handleOpen} endIcon={<AddIcon/>}>
+
+            </Button>
+            
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                <List
+                    sx={{
+                        overflow: 'auto',
+                    }}
+                >
+                    <ListItemText primary='List' />
+                    <Divider/>
+                    {  tags && tags.map(tag=>(
+                        <ListItemText primary={tag.tagName} />
+                    ))}
+                </List>
+                </Box>
+            </Modal>
         </div>
     )
 }
