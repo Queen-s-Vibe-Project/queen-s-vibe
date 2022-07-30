d7b5242bf937a8f998ecaae513134d148c911787
Al Camara <Alhousseinec@gmail.com>
2022-07-30 03:59:13 -0500

    added user

--- src/components/InstructorDetail/InstructorTags.jsx
+++ src/components/InstructorDetail/InstructorTags.jsx
@@ -1,9 +1,13 @@
+import { useSelector, useDispatch } from "react-redux";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 import AddIcon from '@mui/icons-material/Add';
 import AddInstructorTag from "../AddInstructorTag/AddInstructorTag";
 
 function InstructorTags( {tags} ) {
+
+  const user = useSelector((store) => store.user);
+
   return (
     <>
      <div
