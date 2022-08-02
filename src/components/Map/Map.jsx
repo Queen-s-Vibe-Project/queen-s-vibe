42e37b065985081b7ffe3e20d32f92d9a7afe373
Alexander Ratanas <alexanderratanas@gmail.com>
2022-08-02 16:05:00 -0500

    markers on map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import mapStyles from "./mapStyles";
 import "./Map.css";
+import { v4 as uuidv4 } from 'uuid';
+
 
 const containerStyle = {
   width: "325px",
@@ -24,14 +26,14 @@ const Map = ({instructors}) => {
   for (let instructor of instructors) {
     instructor.classes && instructor.classes.map((classer) => {
       // console.log(`latitude ${classer.lat}, longitude ${classer.lng}`)
-      newMarkers.push({ lat: classer.lat, lng: classer.lng })
+      newMarkers.push({ instructor:instructor.id, lat: classer.lat, lng: classer.lng })
     });
   }
   return newMarkers
 }
   
 
-    const [markers, setMarkers] = useState(classMarkers(instructors))
+    const [markers, setMarkers] = useState([])
     const [currentLocation, setCurrentLocation] = useState({
         lat: 39.0997,
         lng: -94.5786
@@ -46,6 +48,9 @@ const Map = ({instructors}) => {
             
         })
     },[])
+    useEffect(() => {
+      setMarkers(classMarkers(instructors))
+    },[instructors])
 
     console.log(markers)
     
@@ -64,12 +69,17 @@ const Map = ({instructors}) => {
         className="map-container"
         mapContainerStyle={containerStyle}
         center={currentLocation}
-        zoom={8}
+        zoom={10}
         options={options}
       >
       {markers.map((marker) => {
       return (
-        <Marker key={marker.lat} position={{ lat: marker.lat, lng: marker.lng}} />
+        <Marker 
+          key={uuidv4()} 
+          position={{ lat: marker.lat, lng: marker.lng}}
+          icon={{
+            
+          }}/>
       )
       })}
       
