6fede566f01b8942b5ce6a61022e1aaad3e2fa0d
Al Camara <Alhousseinec@gmail.com>
2022-08-03 10:32:21 -0500

    got gym goer upcomming classes

--- src/components/UpcomingClasses/UpcomingClasses.jsx
+++ src/components/UpcomingClasses/UpcomingClasses.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react'
+import { useDispatch,useSelector } from 'react-redux'
 import './UpcomingClasses.css'
 import UpcomingClassCard from './UpcomingClassCard'
 
 export default function UpcomingClasses(){
+
+    const user = useSelector((store)=> store.user)
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch({
+            type:'FETCH_UPCOMING_CLASSES',
+            payload: user.id
+        })
+    },[])
+
     return(
         <div>
             <h3>Upcoming Classes</h3>
