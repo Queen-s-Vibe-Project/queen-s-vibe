c3ce9021c59692256f13aef5191b4cb1403144a9
Al Camara <Alhousseinec@gmail.com>
2022-08-04 13:19:26 -0500

    update delete dispatch

--- src/components/InstructorPage/InstructorClassItem.jsx
+++ src/components/InstructorPage/InstructorClassItem.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import "./InstructorPage.css";
 
 function InstructorClassItem({ instructorClass }) {
+
+  const [isDeletePress, setDeletePress] = useState(false)
+  const user = useSelector((store)=> store.user)
+
+  useEffect(()=>{
+
+    if(isDeletePress === true){
+      dispatch({
+        type:"FETCH_INSTRUCTOR_CLASSES",
+        payload: user.id
+      })
+      setDeletePress(false)
+    }
+  },[isDeletePress])
+
+  const dispatch = useDispatch()
+
   return (
     <div className="instructor-card">
       <p>
@@ -19,7 +38,19 @@ function InstructorClassItem({ instructorClass }) {
       </p>
       <div className="class-button">
         <button className="edit-class">Edit</button>
-        <button className="edit-class">Delete</button>
+        <button
+          onClick={()=>{
+            dispatch({
+              type:"DELETE_INSTRUCTOR_CLASS",
+              payload: instructorClass.classId
+            })
+
+            setDeletePress(true)
+          }} 
+          className="edit-class"
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
